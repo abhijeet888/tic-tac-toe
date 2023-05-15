1e4c2386128b5a58ed20f66c876484410f2c7487
abhijeet888 <abhijeet5sharma5@gmail.com>
2023-05-15 16:07:09 +0530

    third commit

--- tictactoe.js
+++ tictactoe.js
@@ -5,6 +5,20 @@ function clickGrid(element) {
     element.innerHTML="0"
  }else{
     element.innerHTML="X"
- } 
- count++;
+   }    
+count++;
+checkWin(element.id);
+}
+
+function checkWin(elementId){
+   console.log("win");
+   const idOne = document.getElementById("grid-column-one");
+   const idTwo = document.getElementById("grid-column-two");
+   const idThree = document.getElementById("grid-column-three");
+   const idFour = document.getElementById("grid-column-four");
+   const idFive = document.getElementById("grid-column-five");
+   const idSix = document.getElementById("grid-column-six");
+   const idSeven = document.getElementById("grid-column-seven");
+   const idEight = document.getElementById("grid-column-eight");
+   const idNine = document.getElementById("grid-column-nine");
 }
