520abb3295bce71818d02f9991e67d375ca94630
Abhijeet Sharma <54783264+abhijeet888@users.noreply.github.com>
2023-05-17 22:41:42 +0530

    changed code structure

--- tictactoe.js
+++ tictactoe.js
@@ -1,24 +1,32 @@
-let count = 1;
-function clickGrid(element) {
- console.log("clicked",element.id);  
- if(count%2==0){
-    element.innerHTML="0"
- }else{
-    element.innerHTML="X"
-   }    
-count++;
-checkWin(element.id);
+const x = "x";
+const zero = "0";
+const winning_combination = [
+   
+
+
+
+   
+   
+
+
+
+   
+]
+
+startGame();
+
+function startGame(){
+   
+
+
+   endGame();
+} 
+
+
+function endGame(){
+    
 }
 
-function checkWin(elementId){
-   console.log("win");
-   const idOne = document.getElementById("grid-column-one");
-   const idTwo = document.getElementById("grid-column-two");
-   const idThree = document.getElementById("grid-column-three");
-   const idFour = document.getElementById("grid-column-four");
-   const idFive = document.getElementById("grid-column-five");
-   const idSix = document.getElementById("grid-column-six");
-   const idSeven = document.getElementById("grid-column-seven");
-   const idEight = document.getElementById("grid-column-eight");
-   const idNine = document.getElementById("grid-column-nine");
+function checkWinningCombination(){
+
 }
